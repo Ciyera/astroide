fb970afcbc8d326602f3c1103a3ac82e01a2fed7
Zineb Dahdouh <zinebdah9494@gmail.com>
2022-04-05 15:34:13 +0200

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+from random import randint
+from if3_game.engine import init, Layer
+from asteroid import RESOLUTION,AsteroidGame, Spaceship, Asteroid, Bullet
+
+
+init(RESOLUTION, (" " * 100) +  "Asteroid!!!!!!!!!!")
+
+
+
+game = AsteroidGame()
+
+
+game.run()
+   
